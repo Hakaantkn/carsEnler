571115883dc98492e2ccfc6d94f9e07517c64625
Hakan Tekin <hakantekin@Hakan-MacBook-Pro.local>
2024-02-10 21:28:48 +0300

    ozan ın araba bitti

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,9 @@
 const modal = document.querySelector(".modal1");
 const openModal = document.querySelector(".open-modal1");
+const openModal2 = document.querySelector(".open-modal2");
+const openModal3 = document.querySelector(".open-modal3");
+const openModal4 = document.querySelector(".open-modal4");
+
 
 
 openModal.addEventListener('click', () => {
