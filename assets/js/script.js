82bdd3dab05620da7cdc93fc930d0e511d24ea70
Hakan Tekin <hakantekin@Hakan-MacBook-Pro.local>
2024-02-11 00:09:39 +0300

    efenin arabada bitti

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,9 @@
 const modal = document.querySelector(".modal1");
 const modal2 = document.querySelector(".modal2");
+const modal3 = document.querySelector(".modal3");
+const modal4 = document.querySelector(".modal4");
+
+
 
 const openModal = document.querySelector(".open-modal1");
 const openModal2 = document.querySelector(".open-modal2");
@@ -8,6 +12,9 @@ const openModal4 = document.querySelector(".open-modal4");
 
 const closeModel1 = document.querySelector(".close-button");
 const closeModel2 = document.querySelector(".close-button2");
+const closeModel3 = document.querySelector(".close-button3");
+const closeModel4 = document.querySelector(".close-button4");
+
 
 
 // modal1 in açılış kapanış kodu
@@ -18,10 +25,27 @@ openModal.addEventListener('click', () => {
 closeModel1.addEventListener('click', () => {
     modal.close();
 })
+
 // modal2 nin açılış kapanışu
 openModal2.addEventListener('click', () => {
     modal2.showModal();
 })
 closeModel2.addEventListener('click', () => {
     modal2.close();
+})
+
+// modal3 ün acılış kapanışu
+openModal3.addEventListener('click', () => {
+    modal3.showModal();
+})
+closeModel3.addEventListener('click', () => {
+    modal3.close();
+})
+
+// modal4 ün açılış kapanışı
+openModal4.addEventListener('click', () => {
+    modal4.showModal();
+})
+closeModel4.addEventListener('click', () => {
+    modal4.close();
 })
